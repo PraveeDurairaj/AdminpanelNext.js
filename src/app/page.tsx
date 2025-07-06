822689711a5582532309da1cb5ea1653b1ad48a4
PraveeDurairaj <duraipraveen8834@gmail.com>
2025-07-06 07:28:00 -0700

    learnings phase 2 part 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import ViewTopics from '@/components/Learnings/ViewTopics';
 import SideMenuLayout from '@/layouts/SideMenuLayout';
 
 
@@ -8,9 +7,8 @@ import SideMenuLayout from '@/layouts/SideMenuLayout';
 export default function Dashboard() {
 
   return (
-    <SideMenuLayout >
-      {/* <h1 className='flex items-center justify-center text-3xl bg-blue-300 p-[15px] h-[500px]  my-auto rounded-[10px] animate-pulse'>work in progress</h1> */}
-      <ViewTopics />
+    <SideMenuLayout title='Dashboard' >
+      <h1 className='flex items-center justify-center text-3xl bg-blue-300 p-[15px] h-[500px]  my-auto rounded-[10px] animate-pulse'>work in progress</h1>
     </SideMenuLayout>
   );
 }
